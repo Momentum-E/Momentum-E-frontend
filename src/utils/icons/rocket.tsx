028f25704162f1d36ec53d1361c82c5edc97aee6
shivam <shivam@momentum-e.com>
2023-06-14 11:54:06 +0530

    🍱 update typos in icon svg

--- src/utils/icons/rocket.tsx
+++ src/utils/icons/rocket.tsx
@@ -5,7 +5,7 @@ const Rocket = () => {
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       viewBox="0 0 16 16"
       height="1em"
       width="1em"
