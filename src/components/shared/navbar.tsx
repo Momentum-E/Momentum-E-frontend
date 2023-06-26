bbd53a5c997e671d66016a2d83ee163fc7f6deda
mannan-momentum=e <mannandassani@momentum-e.com>
2023-06-26 15:05:22 +0530

    modified navbar

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,133 +1,111 @@
-import { Disclosure } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 import logo from '../../assets/logos/logo_white_nocap.png';
-import Link from 'next/link';
 import Image from 'next/image';
-import { Status } from '../auth';
-import { Account } from '../auth/account';
+import Link from 'next/link';
+export default function NavBar() {
+  const [navbar, setNavbar] = useState(false);
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
-}
-
-const Navbar = () => {
   return (
-    <Account>
-      <Disclosure as="nav" className="relative z-10">
-        {({ open }) => (
-          <>
-            <div className=" max-w-7xl px-2 sm:px-6 lg:px-16 text-white-100">
-              <div className="relative flex h-16 items-center justify-between">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                  {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-                <div className="flex sm:flex-none items-center justify-center w-full sm:justify-between">
-                  <div className="flex items-center justify-center">
-                    {/* expanded image */}
-                    <Image
-                      className="block h-8 w-auto lg:hidden"
-                      src={logo}
-                      alt="Momentum-E"
-                      height={100}
-                      width={100}
+    <nav className="w-full bg-purple-500 shadow">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <Link href="/">
+              <h2 className="text-2xl font-bold text-white-100">
+                <figure>
+                  <Image src={logo} alt="Logo" height={100} width={100} />
+                </figure>
+              </h2>
+            </Link>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}>
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-white-100"
+                    viewBox="0 0 20 20"
+                    fill="currentColor">
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
                     />
-                    {/* reduced image */}
-                    <Image
-                      className="hidden h-8 w-auto lg:block"
-                      src={logo}
-                      alt="Momentum-e"
-                      height={100}
-                      width={100}
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-white-100"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
                     />
-                  </div>
-                  <div className="hidden sm:ml-6 sm:block">
-                    <div className="flex space-x-4">
-                      <Link
-                        href="/"
-                        className={
-                          'hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium'
-                        }>
-                        Home
-                      </Link>
-                      <Link
-                        href="/aboutus"
-                        className={
-                          'hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium'
-                        }>
-                        About Us
-                      </Link>
-                      <Status
-                        classname={
-                          'hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium'
-                        }
-                        href="/auth/login"
-                      />
-                      <Link
-                        href="/auth/register"
-                        className={
-                          'hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium'
-                        }>
-                        Register
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
+                  </svg>
+                )}
+              </button>
             </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`justify-self-center pb-3 lg:hidden mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? 'block' : 'hidden'
+            }`}>
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              <li className="text-white-100 hover:text-indigo-200">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="text-white-100 hover:text-indigo-200">
+                <Link href="/about-us">About Us</Link>
+              </li>
+              {/* <li className="text-white-100 hover:text-indigo-200">
+                <Link href="/contact-us">Contact US</Link>
+              </li> */}
+            </ul>
 
-            <Disclosure.Panel className="sm:hidden">
-              <ul className="space-y-1 px-2 pb-3 pt-2">
-                <Disclosure.Button as="li">
-                  <Link
-                    href="/"
-                    className= 'text-white-100 hover:bg-gray-700 hover:text-white-200 block rounded-md px-3 py-2 text-base font-bold'
-                    >
-                    Home
-                  </Link>
-                </Disclosure.Button>
-                <Disclosure.Button as="li">
-                  <Link href="/#aboutus">
-                    <span
-                      className={classNames(
-                        ' text-white-100 hover:bg-gray-700 hover:text-white-200 rounded-md px-3 py-2 text-base font-medium'
-                      )}>
-                      About Us
-                    </span>
-                  </Link>
-                </Disclosure.Button>
-                <Disclosure.Button as="li">
-                  <Link
-                    href="/auth/login"
-                    className={classNames(
-                      'text-white-100 hover:bg-gray-700 hover:text-white-200 block rounded-md px-3 py-2 text-base font-medium'
-                    )}>
-                    Login
-                  </Link>
-                </Disclosure.Button>
-                <Disclosure.Button as="li">
-                  <Link
-                    href="/auth/register"
-                    className={classNames(
-                      'text-white-100 hover:bg-gray-700 hover:text-white-200 block rounded-md px-3 py-2 text-base font-medium'
-                    )}>
-                    Register
-                  </Link>
-                </Disclosure.Button>
-              </ul>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
-    </Account>
+              <div className="mt-3 space-y-2 lg:hidden md:inline-block">
+                <Link
+                  href="/auth/login"
+                  className="inline-block w-full px-4 py-2 text-center text-white-100 bg-gray-600 rounded-md shadow hover:bg-gray-800">
+                  Login
+                </Link>
+                <Link
+                  href="/auth/register"
+                  className="inline-block w-full px-4 py-2 text-center text-white-100 bg-white rounded-md shadow hover:bg-gray-100">
+                  Register
+                </Link>
+              </div>
+          </div>
+        </div>
+        <div className="hidden space-x-2 md:inline-block">
+          <Link
+            href="/"
+            className="px-4 py-2 text-white-100 bg-gray-600 rounded-md shadow hover:bg-gray-800">
+            Home
+          </Link>
+          <Link
+            href="/#aboutus"
+            className="px-4 py-2 text-white-100 bg-white rounded-md shadow hover:bg-gray-100">
+            About Us
+          </Link>
+          <Link
+            href="/auth/login"
+            className="px-4 py-2 text-white-100 bg-gray-600 rounded-md shadow hover:bg-gray-800">
+            Login
+          </Link>
+          <Link
+            href="/auth/register"
+            className="px-4 py-2 text-white-100 bg-white rounded-md shadow hover:bg-gray-100">
+            Register
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
-};
-
-export default Navbar;
+}
