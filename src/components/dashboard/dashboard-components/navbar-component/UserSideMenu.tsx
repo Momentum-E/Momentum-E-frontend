2c598f6d3c9777c51b736a104c3c06ac3f6efa84
mannan-momentum=e <mannandassani@momentum-e.com>
2023-07-13 18:53:43 +0530

    Added light-dark theme toggle

--- src/components/dashboard/dashboard-components/navbar-component/UserSideMenu.tsx
+++ src/components/dashboard/dashboard-components/navbar-component/UserSideMenu.tsx
@@ -122,22 +122,15 @@ const UserSideMenu:React.FC<UserSideMenuProps> = ({
                 <Menu.Item>
                 {({ active }) => (
                     <li
-                    onClick={() => SignOut()}
-                    className={classNames(
-                        active ? 'bg-gray-100' : '',
-                        'block px-4 py-2 text-sm rounded-md text-black hover:bg-white-200 hover:cursor-pointer'
-                    )}>
-                    <p className="">
-                        Theme:
-                    </p>
-                    <div className="bg-white-100 absolute z-[999] bottom-0 right-0 -translate-x-1/2 -translate-y-1/2 rounded-full dark:bg-gray-900">
+                    className={'block px-4 py-2 text-sm rounded-md text-black flex items-center justify-center'}>
+                    <div className="bg-white-100 rounded-full dark:bg-gray-900">
                         <div className="flex flex-wrap items-center justify-between">
                             <div className="flex md:order-2">
                                 <ul className="flex flex-row p-2 md:space-x-8 md:mt-0 md:text-sm md:font-medium">            
                                 <li>
                                     <button className="block rounded md:p-0" onClick={()=> setTheme( theme === "dark"? "light": "dark" )}>
                                     { theme==="dark"? 
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white-100">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
                                     </svg>
                                     : 
