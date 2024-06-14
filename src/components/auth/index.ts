861c2624a72cab190a880c7508b63532f94aee9f
Bhupendra <bhupendra@momentum-e.com>
2024-06-14 17:35:04 +0530

    signup export bug fix

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,5 +1,5 @@
-export { default as SignUp } from './Signup';
-export { default as SignIn } from './Signin';
+export { default as SignUp } from './signup';
+export { default as SignIn } from './signin';
 export {default as ProtectedRoute} from './ProtectedRoute'
 export { default as ForgotPassword } from './ForgotPassword'
 export {default as GetUserDataComponent} from './GetUserDataComponent'
