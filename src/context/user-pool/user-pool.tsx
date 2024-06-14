4ce356caa5acfddac6c71b503560d0ade587a87f
Bhupendra <bhupendra@momentum-e.com>
2024-06-14 18:08:25 +0530

    updated usePool

--- src/context/user-pool/user-pool.tsx
+++ src/context/user-pool/user-pool.tsx
@@ -1,9 +1,17 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import { CognitoUserPool } from "amazon-cognito-identity-js";
+
+const userPoolId = process.env.NEXT_PUBLIC_COGNITO_USERPOOL_ID;
+const clientId = process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID;
+
+if (!userPoolId || !clientId) {
+  throw new Error(
+    "Missing Cognito User Pool ID or Client ID in environment variables"
+  );
+}
 
 const poolData = {
-  UserPoolId: 'ap-south-1_0MbT9rHLh',
-  // Region: 'ap-south-1',
-  ClientId: '695jnckdbn931ctp64k8iop33s',
+  UserPoolId: userPoolId as string,
+  ClientId: clientId as string,
 };
 
 export default new CognitoUserPool(poolData);
